0474239309dc43861237cb322c246ce57cfb3b4c
kozue_noro <kozuenoro1207@gmail.com>
2015-11-19 08:08:42 +0000

    [add] posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts
   end
   def new
     @user=User.new
