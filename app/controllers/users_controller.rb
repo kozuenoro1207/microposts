bec51a317b13fe8e000e58adb9ec19a325da8757
kozue_noro <kozuenoro1207@gmail.com>
2015-11-19 06:42:44 +0000

    [add] edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,33 @@
 class UsersController < ApplicationController
-  def show 
-   @user = User.find(params[:id])
+  before_action :logged_in_user, only: [:edit, :update]
+  def show
+    @user = User.find(params[:id])
   end
   def new
     @user=User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App"
-      redirect_to @user 
+      redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(user_params)
+      flash[:info] = "Your profile has been saved."
+      redirect_to user_url(@user)
+    else
+      render :edit
     end
   end
 
@@ -20,6 +35,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:area, :bio)
   end
 end
